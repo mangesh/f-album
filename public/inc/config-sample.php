5d53f669698d10dd6cd84a51963c37a347c9f3d4
root at myhost <root@localhost.localdomain>
2015-09-02 00:02:15 +0530

    updated config sample

--- public/inc/config-sample.php
+++ public/inc/config-sample.php
@@ -9,11 +9,11 @@
     $fb = array(
             'app_id'        => '',
             'app_secret'    => '',
-            'callback_url'  => ''
+            'callback_url'  => ''   //http://<your-domain>/callback
         );
 
     $google = array(
             'client_id'         => '',
             'client_secret'     => '',
-            'callback_url'      => ''
+            'callback_url'      => '' //http://<your-domain>/google_callback
         );
--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 set_time_limit(0);
 session_start();
-
+date_default_timezone_set('Asia/Calcutta');
 // Load Composer's PSR-4 autoloader (necessary to load Slim, Mini etc.)
 require '../lib/vendor/autoload.php';
 
